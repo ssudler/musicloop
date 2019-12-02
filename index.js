64e720fbcff9c6c6f10c1768e4e522f7bfeb0d20
ssudler <ssudler21@gmail.com>
2019-12-01 23:54:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('User connected');
+  socket.on('disconnect', function() {
+    console.log('User disconnected');
+  })
+})
+
+http.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
